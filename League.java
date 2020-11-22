9b921586a40779ccb6529efe656a5be8d5198f39
MiaNguyen11579 <nnqnguyen@mun.ca>
2020-11-22 16:19:22 +0700

    Update2

--- League.java
+++ League.java
@@ -1,3 +1,4 @@
+
     import java.util.*;
     /**
      * The League class manages the whole league. from here you can add matches 
@@ -7,7 +8,7 @@
      */
     public class League
     {
-        // instance variables - replace the example below with your own
+        // instance variables
         private ArrayList<Team> teamList;
         private int players;
         private int coaches;
@@ -26,8 +27,7 @@
         }
         
         /**
-         * simple method to create teams and adds them to divisions.
-         *
+         * Simple method to create teams and adds them to divisions.
          * 
          */
         public void leagueSetup()
@@ -40,14 +40,14 @@
         }
         
         /**
-         * simple method to check if both teams are in the same division should return true or false
+         * Simple method to check if both teams are in the same division should return true or false
          *
          * @param  y  a sample parameter for a method
          * @return    boolean
          */
         private boolean checkDivision(Team team1, Team team2)
         {
-            if (team1.getDivision() == team2.getDivision()) 
+            if (team1.getDivision().equals(team2.getDivision()))
             {
                 return true;
             }
@@ -64,7 +64,7 @@
          * @param  divisionName  name of the division
          * @return list of teams in the same division
          */
-        private ArrayList<Team> getTeamsInDivision(int divisionName)
+        private ArrayList<Team> getTeamsInDivision(String divisionName)
         {
             ArrayList<Team> divisionTeam = new ArrayList();
             for (Team team : teamList) 
@@ -76,16 +76,87 @@
             }
             return divisionTeam;
         }
+        
+        /**
+         * Starts a tournament based on the League setup, creates matches between teams in the same division.
+         *
+         */
+        public void tournament()
+        {
+            Random rand = new Random();
+            for (int x = 1; x < 4; x++)
+            {
+                ArrayList<Team> divisionTeam = getTeamsInDivision("Division " + x);
+                
+                while (divisionTeam.size() != 1)
+                {
+                    
+                    for (int i = 1; i < divisionTeam.size(); i++)
+                    {
+                        ArrayList<ArrayList<Integer>> scoreList = createScore();
+                        System.out.println(divisionTeam.get(0).getName() + " : " + divisionTeam.get(i).getName());
+                        System.out.println(scoreList); // I put this and the line above to check the content of scoreList, remove if you want.
+                        addMatch(divisionTeam.get(0),divisionTeam.get(i),scoreList.get(0),scoreList.get(1));
+                        
+                    }
+                    divisionTeam.remove(0);
+                }
+            }
+        }
+        
+        /**
+         * Generate two lists of scores for two teams.
+         *
+         */
+        private ArrayList<ArrayList<Integer>> createScore()
+        {
+            score1 = new ArrayList();
+            score2 = new ArrayList();
+            int point1 = 0;
+            int point2 = 0;
+            int maxScore = 21;
+            Random rand = new Random();
+                for (int i = 0; i < 5; i++)
+                {
+                    // If reaches 5th match.
+                    if (i == 4) 
+                    {
+                        maxScore = 15;
+                    }
+                    
+                    if (rand.nextInt(2) == 0)
+                    {
+                        score1.add(maxScore);
+                        score2.add(rand.nextInt(maxScore));
+                        point1++;
+                    }
+                    else 
+                    {
+                        score2.add(maxScore);
+                        score1.add(rand.nextInt(maxScore));
+                        point2++;
+                    }
+                    // If one of the teams reaches 3 points.
+                    if (point1 == 3 || point2 == 3) 
+                    {
+                        break;
+                    }
+                    
+                }
+            ArrayList<ArrayList<Integer>> arr = new ArrayList();
+            arr.add(score1);
+            arr.add(score2);
+            return arr;
+        }
     
     
         /**
          * Add a match and all relevant scores to the teams participating
          * Checks if both teams are in the same division, if they aren't, does nothing
          *
-         * @param  y  a sample parameter for a method
-         * @return    the sum of x and y
+         * 
          */
-        public void addMatch(Team team1, Team team2, 
+        private void addMatch(Team team1, Team team2, 
                              ArrayList<Integer> score1, ArrayList<Integer> score2)
         {
             if (!checkDivision(team1,team2))
@@ -96,10 +167,10 @@
             int point1 = 0;
             int point2 = 0;
             
-            for (int x = 0; x < 6; x++)
+            for (int x = 0; x < score1.size(); x++)
              {
                 int pointDiff = score1.get(x) - score2.get(x);
-                 if (pointDiff > 0)
+                if (pointDiff > 0)
                 {
                     team1.updateLeaguePoints();
                     point1++;
@@ -141,11 +212,12 @@
          * @param  y  a sample parameter for a method
          * @return    the sum of x and y
          */
-        public void calculateStanding(int divisionName)
+        public void calculateStanding(String divisionName)
         {
-            Map<Integer, String> pointList = new TreeMap<>();
+            Map<Integer, String> pointList = new TreeMap<>((Comparator<Integer>) (o1, o2) -> o2.compareTo(o1));
+            ArrayList<Team> List = getTeamsInDivision(divisionName);
         
-            for (Team team : getTeamsInDivision(divisionName))
+            for (Team team : List)
             {
                 pointList.put(team.getLeaguePoints(),team.getName());
             }
